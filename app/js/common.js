3b91ed3997d1f28d94486c1f0a2ff1ae436e73d0
evdeveloper <mushnikovz@mail.ru>
2021-12-27 12:04:49 +0300

    Сделал адаптив и добавил плеер

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
 function fixedMenu() {
 	let nav = $('.header');
@@ -15,12 +15,58 @@ function fixedMenu() {
 fixedMenu();
 
 const swiper = new Swiper('.swiper', {
-    autoplay: { delay: 5000 },
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  autoplay: { delay: 5000 },
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
   
 });
+
+function sandwich() {
+  let body = $('body');
+  let overlay = $('.overlay');
+  $(document).on('click', '.sandwich', function(e) {
+    $(this).toggleClass('active');
+    if (!$(this).hasClass('active')) {
+      $('.header__menu').removeClass('active');
+      $(body).removeClass('hidden');
+      $(overlay).fadeOut();
+    }else {
+      $('.header__menu').addClass('active');
+      $(body).addClass('hidden');
+      $(overlay).fadeIn();
+    }
+    return false;
+  });
+
+};
+
+sandwich();
+
+$(document).on('click', '.overlay', function(e) {
+  $(this).fadeOut();
+  $('.header__menu').removeClass('active');
+  $('body').removeClass('hidden');
+  $('.sandwich').removeClass('active');
+  return false;
+});
+
+let eventPlaeer = $(".video");
+
+eventPlaeer.RTOP_VideoPlayer({
+  fontAwesomeControlIcons: true,
+  showSoundControl: true,
+  allowPlayPause:true
+});
+
+eventPlaeer.on('play.vid.rtop_videoplayer', function(event) {
+  console.log(event);
+  $('.rtopVideoPlayerWrapper .rtopVideoPlayer .rtopVideoHolder video').css({'opacity': '1'});
+})
+
+
+
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -49,7 +49,8 @@ gulp.task('styles', function() {
 gulp.task('scripts', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js', // Always at the end
+		'app/libs/video/rtop.videoPlayer.1.0.2.min.js',
+		'app/js/common.js',
 		])
 	.pipe(concat('scripts.min.js'))
 	.pipe(uglify({ output: { comments: false } }))
